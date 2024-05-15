b1c7ed69b22fc05b00b2225af2cd3078aca82618
zarrafkhan <zarraf.khan@students.mq.edu.au>
2024-05-15 10:37:43 +1000

    simp-api

---
+++ simp-api/go.mod
@@ -0,0 +1,3 @@
+module example.com/simp-api
+
+go 1.22.1
---
+++ simp-api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "os"
+
+//"fmt"
+//"os"
+
+func main() {
+
+	os.Exit(0)
+}
+
+// func fizzbuzz(i int) {
+//     if i % 5 == 0 && i % 3 == 0 {
+//         fmt.Print(" FizzBuzz")
+//     } else if i % 3 == 0 {
+//         fmt.Print(" Fizz")
+//     } else if i % 5 == 0 {
+//         fmt.Print(" Buzz")
+//     } else {
+//     fmt.Print(" ",i)
+//     }
+// }
+
+// func isMT[T any](sl []T) bool{
+//     return len(sl)==0
+// }
+
+// func sqrt[T int | float32 | float64](sl []T) T{
+//     var sq T
+//     return sq
+// }
